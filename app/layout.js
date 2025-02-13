feade6be958d195f9b67db6eeb96ce6607dc0bcb
zephyr-dassouli <zephyrdassouli@gmail.com>
2025-02-13 15:00:08 +0100

    Add: grain background and twaek css window close

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="font-custom">
+      <body className="font-custom tv-grain">
         <main className="bg-background w-screen h-screen overflow-hidden fixed top-0 left-0 flex flex-col ">
           <Header />
           {children}
