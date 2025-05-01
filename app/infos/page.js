79681d4a0932d1123ed42dc869aac4c1e57a997e
Zephyr Dassouli <zephyr.dassouli@imt-atlantique.net>
2025-05-01 12:47:54 +0200

    Fix : Infos page changes

--- app/infos/page.js
+++ app/infos/page.js
@@ -6,19 +6,25 @@ import AsciiWaveAnimation from '@/components/three/AsciiWaveAnimation';
 export default function Infos() {
   return (
     <div className="flex flex-row gap-4 items-center w-full md:pl-20">
-      <div className="relative px-5 pt-6 md:px-8 md:pt-20 h-full md:top-0 md:w-[42vw] overflow-y-auto thin-scrollbar bg-background overflow-x-hidden flex flex-row gap-4">
-        <div className="flex flex-col gap-4 text-right text-sm md:text-lg md:w-[10vw]" style={{whiteSpace: 'nowrap'}}>
-          <div className=" text-pblue">[NAME]</div>
-          <div className=" text-pblue">[AGE]</div>
-          <div className=" text-pblue">[LOCATION]</div>
-          <div className=" text-pblue">[DETAILS]</div>
+      <div className="text-sm md:text-lg flex flex-col gap-8 md:gap-16 md:top-0 md:w-[42vw] px-5 pt-6 pb-6 bg-background md:px-8 md:pt-20  md:pb-20  overflow-y-auto thin-scrollbar overflow-x-hidden">
+        <div className="relative h-full flex flex-row gap-4">
+          <div className="flex flex-col gap-4 text-right md:w-[10vw]" style={{ whiteSpace: 'nowrap' }}>
+            <div className=" text-pblue">[NAME]</div>
+            <div className=" text-pblue">[AGE]</div>
+            <div className=" text-pblue">[LOCATION]</div>
+          </div>
+          <div className="flex flex-col gap-4 md:max-w-[30vw]">
+            <RandomTextReveal className={'text-foreground'} text={'Zéphyr Dassouli'} />
+            <RandomTextReveal className={'text-foreground'} text={'21'} />
+            <RandomTextReveal className={'text-foreground'} text={'Toulouse, France'} />
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-sm md:text-lg md:max-w-[30vw]">
-          <RandomTextReveal className={'text-foreground'} text={'Zéphyr Dassouli'} />
-          <RandomTextReveal className={'text-foreground'} text={'21'} />
-          <RandomTextReveal className={'text-foreground'} text={'Toulouse, France'} />
-          <RandomTextReveal className={'text-foreground'} text={"I am a master's student in CompSci at IMT Atlantique with a strong interest in cybersecurity and software development"} />
-          <RandomTextReveal className={'text-foreground '} text={'Currently, I am doing a research internship at the University of Amsterdam in the Multiscale Networked Systems group for a duration of 4 months. My research focuses on blockchain optimization to secure IoT devices'} />
+        <div>
+          <div className="text-center text-background bg-pblue">[DETAILS]</div>
+          <div className="border md:text-base flex flex-col gap-4 p-3 md:p-6 border-pblue overflow-hidden">
+            <RandomTextReveal className={'text-foreground'} text={"I am a master's student in CompSci at IMT Atlantique with a strong interest in cybersecurity and software development"} />
+            <RandomTextReveal className={'text-foreground '} text={'Currently, I am doing a research internship at the University of Amsterdam in the Multiscale Networked Systems group. My research focuses on blockchain optimization to secure IoT devices'} />
+          </div>
         </div>
       </div>
       <div className=" opacity-25 md:opacity-100 w-[30vw] absolute top-0 md:right-[45vw]">
